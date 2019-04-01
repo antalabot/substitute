a2816d282ea94606b32c9b16cdfc929385c0737a
Piotr Sowa <mail@sowiszcze.pl>
2019-04-01 18:50:06 +0200

    Fixed bot command responses

--- Substitute.Bot/DiscordBot.cs
+++ Substitute.Bot/DiscordBot.cs
@@ -1,6 +1,7 @@
-﻿using Discord.WebSocket;
+﻿using Discord;
+using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
-using Substitute.Domain;
+using Substitute.Bot.Models;
 using Substitute.Domain.Data.Entities;
 using Substitute.Domain.DataStore;
 using Substitute.Domain.Enums;
@@ -31,8 +32,9 @@ namespace Substitute.Bot
 
         public async Task LoginAndStart()
         {
-            await _client.LoginAsync(Discord.TokenType.Bot, _configuration[DISCORD_TOKEN_KEY]);
+            await _client.LoginAsync(TokenType.Bot, _configuration[DISCORD_TOKEN_KEY]);
             await _client.StartAsync();
+            await _client.SetActivityAsync(new BotActivity { Name = "Bits and Bytes", Type = ActivityType.Playing });
         }
 
         private void Configure()
@@ -46,19 +48,19 @@ namespace Substitute.Bot
             if (!message.Author.IsBot && !string.IsNullOrWhiteSpace(command) && command.StartsWith('>'))
             {
                 command = command.Substring(1).ToLower();
-                ImageResponse image = _context.Get<ImageResponse>().SingleOrDefault(i => i.Command == command);
+                SocketGuild guild = _client.Guilds.Single(g => g.Channels.Any(c => c.Id == message.Channel.Id));
+                ImageResponse image = _context.Get<ImageResponse>().SingleOrDefault(i => i.Command == command && i.GuildId == guild.Id);
                 if (image != null)
                 {
-                    byte[] data = await _storage.Get(EStorage.ImageResponse, image.Fielename, image.GuildId.GetValueOrDefault().ToString());
-                    using (MemoryStream stream = new MemoryStream(data))
+                    using (message.Channel.EnterTypingState())
                     {
-                        await message.Channel.SendFileAsync(stream, image.Fielename);
+                        byte[] data = await _storage.Get(EStorage.ImageResponse, image.Fielename, image.GuildId.GetValueOrDefault().ToString());
+                        using (MemoryStream stream = new MemoryStream(data))
+                        {
+                            await message.Channel.SendFileAsync(stream, image.Fielename);
+                        }
                     }
                 }
-            }
-            if (message.Content == "!ping")
-            {
-                await message.Channel.SendMessageAsync("Pong!");
             }
         }
     }
---
+++ Substitute.Bot/Models/BotActivity.cs
@@ -0,0 +1,11 @@
+﻿using Discord;
+
+namespace Substitute.Bot.Models
+{
+    public class BotActivity : IActivity
+    {
+        public string Name { get; set; }
+
+        public ActivityType Type { get; set; }
+    }
+}
